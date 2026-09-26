1820aab69547e1508e18e3ed618a61a1f3847fc7
wan <oops011@163.com>
2026-09-26 15:57:49 +0800

    test: add editor module tests

--- tests/test_editor.c
+++ tests/test_editor.c
@@ -1,5 +1,7 @@
 #include "../editor.h"
 
+#include <string.h>
+
 
 int test_editor_init()
 {
@@ -18,6 +20,164 @@ int test_editor_init()
 
     if(editor.modified != 0)
         return 0;
+
+    return 1;
+}
+
+int test_insert_char()
+{
+    struct Editor editor;
+
+    editor_init(&editor);
+
+    if(!editor_add_line(&editor, "abc")) return 0;
+
+    if(!editor_insert_char(&editor, 0, 1, 'X'))
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    if(strcmp(editor.lines[0], "aXbc") != 0)
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    editor_destroy(&editor);
+
+    return 1;
+}
+
+int test_delete_char()
+{
+    struct Editor editor;
+
+    editor_init(&editor);
+
+    if(!editor_add_line(&editor, "abc")) return 0;
+
+    if(!editor_delete_char(&editor, 0, 2))
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    if(strcmp(editor.lines[0], "ac") != 0)
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    editor_destroy(&editor);
+
+    return 1;
+}
+
+int test_delete_forward_char()
+{
+    struct Editor editor;
+
+    editor_init(&editor);
+
+    if(!editor_add_line(&editor, "abc")) return 0;
+
+    if(!editor_delete_forward_char(&editor, 0, 1))
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    if(strcmp(editor.lines[0], "ac"))
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    editor_destroy(&editor);
+
+    return 1;
+}
+
+int test_insert_newline()
+{
+    struct Editor editor;
+
+    editor_init(&editor);
+    
+    if(!editor_add_line(&editor, "Hello World")) return 0;
+
+    if(!editor_insert_newline(&editor, 0, 5))
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    if(strcmp(editor.lines[0], "Hello") != 0 || strcmp(editor.lines[1], " World") != 0)
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+    editor_destroy(&editor);
+
+    return 1;
+}
+
+int test_save_load()
+{
+    struct Editor editor;
+
+    editor_init(&editor);
+
+    editor_add_line(&editor,"hello");
+    editor_add_line(&editor,"world");
+
+
+    if(!editor_save(&editor,"test.txt"))
+    {
+        editor_destroy(&editor);
+        return 0;
+    }
+
+
+    editor_destroy(&editor);
+
+
+    struct Editor loaded;
+
+    editor_init(&loaded);
+
+
+    if(!editor_load(&loaded,"test.txt"))
+    {
+        editor_destroy(&loaded);
+        return 0;
+    }
+
+
+    if(loaded.size != 2)
+    {
+        editor_destroy(&loaded);
+        return 0;
+    }
+
+
+    if(strcmp(loaded.lines[0],"hello") != 0)
+    {
+        editor_destroy(&loaded);
+        return 0;
+    }
+
+
+    if(strcmp(loaded.lines[1],"world") != 0)
+    {
+        editor_destroy(&loaded);
+        return 0;
+    }
+
+
+    editor_destroy(&loaded);
 
     return 1;
 }
--- tests/test_runner.c
+++ tests/test_runner.c
@@ -2,7 +2,11 @@
 
 
 int test_editor_init();
-
+int test_insert_char();
+int test_delete_char();
+int test_delete_forward_char();
+int test_insert_newline();
+int test_save_load();
 
 int main()
 {
@@ -21,6 +25,61 @@ int main()
     else
     {
         printf("[FAIL] editor_init\n");
+        failed++;
+    }
+
+    if(test_insert_char())
+    {
+        printf("[PASS] editor_insert\n");
+        passed++;
+    }
+    else
+    {
+        printf("[FAIL] editor_insert\n");
+        failed++;
+    }
+
+    if(test_delete_char())
+    {
+        printf("[PASS] editor_delete\n");
+        passed++;
+    }
+    else
+    {
+        printf("[FAILED] editor_delete\n");
+        failed++;
+    }
+
+    if(test_delete_forward_char())
+    {
+        printf("[PASS] editor_delete_forward\n");
+        passed++;
+    }
+    else
+    {
+        printf("[FAILED] editor_delete_forward\n");
+        failed++;
+    }
+
+    if(test_insert_newline())
+    {
+        printf("[PASS] editor_insert\n");
+        passed++;
+    }
+    else
+    {
+        printf("[FAILED] editor_insert\n");
+        failed++;
+    }
+
+    if(test_save_load())
+    {
+        printf("[PASS] editor_save_load\n");
+        passed++;
+    }
+    else
+    {
+        printf("[FAILED] editor_save_load\n");
         failed++;
     }
 
